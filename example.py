67fc5ab1c91d530ad104d9a94ebf23562bf292f9
jconez97 <93310214+jconez97@users.noreply.github.com>
2021-10-28 14:51:40 +0800

    Update example.py

--- example.py
+++ example.py
@@ -9,7 +9,7 @@ try:
         perc = mq.MQPercentage()
         sys.stdout.write("\r")
         sys.stdout.write("\033[K")
-        sys.stdout.write("LPG: %g ppm, CO: %g ppm, Smoke: %g ppm" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
+        sys.stdout.write("NH4: %g ppm, CO: %g ppm, CO2: %g ppm" % (perc["GAS_NH4"], perc["CO"], perc["CO2"]))
         sys.stdout.flush()
         time.sleep(0.1)
 
